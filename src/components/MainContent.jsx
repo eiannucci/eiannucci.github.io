13c840a7fb122d5bea1e744d8c28975c0a126f7f
eiannucci-studioscience <e.iannucci@studioscience.com>
2025-10-24 10:56:42 -0400

    test

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -37,7 +37,7 @@ let btnList = [
     image: true,
   },
   {
-    img: "https://humor.undergroundshirts.com/cdn/shop/products/DostThouEvenHoist-htrMilitary_2_1024x1024@2x.jpg?v=1575391519",
+    img: CubeWave,
     title: "Shakespearian Insult Generator",
     desc: "Insult Thyself...",
     id: "InsultGenerator",
